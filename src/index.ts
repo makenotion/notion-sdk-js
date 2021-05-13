26d71f907de9d2997dde47765f52629a632a93c3
Ankur Oberoi <aoberoi@gmail.com>
2021-05-13 03:11:54 -0700

    exports loglevel

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export { default as Client } from './Client';
+export { LogLevel, Logger } from './logging'
 export { APIErrorCode, APIResponseError, HTTPResponseError, RequestTimeoutError } from './errors';
