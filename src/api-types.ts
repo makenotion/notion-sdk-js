f8d03d952c53c91e409d5418a16333f5a3740c1c
Ivo <16694197+ivoberger@users.noreply.github.com>
2021-05-14 23:39:39 +0200

    fixes #58: block types

--- src/api-types.ts
+++ src/api-types.ts
@@ -5,7 +5,6 @@
  * In the future, the contents of this file will be generated from an API definition.
  */
 
-
 /*
  * Pagination
  */
@@ -15,9 +14,11 @@ export interface PaginationParameters {
   page_size?: number;
 }
 
-export interface PaginatedList<O extends APISingularObject = APISingularObject> {
-  object: 'list',
-  results: O[],
+export interface PaginatedList<
+  O extends APISingularObject = APISingularObject
+> {
+  object: "list";
+  results: O[];
   has_more: boolean;
   next_cursor: string | null;
 }
@@ -47,9 +48,8 @@ export type Block =
   | ChildPageBlock
   | UnsupportedBlock;
 
-
 export interface BlockBase {
-  object: 'block';
+  object: "block";
   id: string;
   type: string;
   created_time: string;
@@ -58,61 +58,61 @@ export interface BlockBase {
 }
 
 export interface ParagraphBlock extends BlockBase {
-  type: 'paragraph';
-  text: RichText[];
+  type: "paragraph";
+  paragraph: { text: RichText[] };
   children?: BlockBase[];
 }
 
 export interface HeadingOneBlock extends BlockBase {
-  type: 'heading_1';
-  text: RichText[];
+  type: "heading_1";
+  heading_1: { text: RichText[] };
   has_children: false;
 }
 
 export interface HeadingTwoBlock extends BlockBase {
-  type: 'heading_2';
-  text: RichText[];
+  type: "heading_2";
+  heading_2: { text: RichText[] };
   has_children: false;
 }
 
 export interface HeadingThreeBlock extends BlockBase {
-  type: 'heading_3';
-  text: RichText[];
+  type: "heading_3";
+  heading_2: { text: RichText[] };
   has_children: false;
 }
 
 export interface BulletedListItemBlock extends BlockBase {
-  type: 'bulleted_list_item';
-  text: RichText[];
+  type: "bulleted_list_item";
+  bulleted_list_item: { text: RichText[] };
   children?: BlockBase[];
 }
 
 export interface NumberedListItemBlock extends BlockBase {
-  type: 'numbered_list_item';
-  text: RichText[];
+  type: "numbered_list_item";
+  numbered_list_item: { text: RichText[] };
   children?: BlockBase[];
 }
 
 export interface ToDoBlock extends BlockBase {
-  type: 'to_do';
-  text: RichText[];
+  type: "to_do";
+  to_do: { text: RichText[] };
   checked: boolean;
   children?: BlockBase[];
 }
 
 export interface ToggleBlock extends BlockBase {
-  type: 'toggle';
-  text: RichText[];
+  type: "toggle";
+  toggle: { text: RichText[] };
   children?: BlockBase[];
 }
 
 export interface ChildPageBlock extends BlockBase {
-  type: 'child_page';
-  title: string;
+  type: "child_page";
+  child_page: { title: string };
 }
 
 export interface UnsupportedBlock extends BlockBase {
-  type: 'unsupported';
+  type: "unsupported";
 }
 
 /*
@@ -120,12 +120,12 @@ export interface UnsupportedBlock extends BlockBase {
  */
 
 export interface Database {
-  object: 'database';
+  object: "database";
   id: string;
   created_time: string;
   last_edited_time: string;
   title: RichText[];
-  properties: { [propertyName: string] : Property };
+  properties: { [propertyName: string]: Property };
 }
 
 /*
@@ -159,111 +159,135 @@ export interface PropertyBase {
 }
 
 export interface TitleProperty extends PropertyBase {
-  type: 'title';
+  type: "title";
   title: Record<string, never>;
 }
 
 export interface RichTextProperty extends PropertyBase {
-  type: 'rich_text';
+  type: "rich_text";
   rich_text: Record<string, never>;
 }
 
 export interface NumberProperty extends PropertyBase {
-  type: 'number';
+  type: "number";
   number: {
-    format: 'number' | 'number_with_commas' | 'percent' | 'dollar' | 'euro' | 'pound' | 'yen' | 'ruble' | 'rupee' | 'won' | 'yuan';
-  }
+    format:
+      | "number"
+      | "number_with_commas"
+      | "percent"
+      | "dollar"
+      | "euro"
+      | "pound"
+      | "yen"
+      | "ruble"
+      | "rupee"
+      | "won"
+      | "yuan";
+  };
 }
 
 export interface SelectProperty extends PropertyBase {
-  type: 'select';
+  type: "select";
   select: SelectOption[];
 }
 
 export interface MultiSelectProperty extends PropertyBase {
-  type: 'multi_select';
+  type: "multi_select";
   multi_select: MultiSelectOption[];
 }
 
 export interface DateProperty extends PropertyBase {
-  type: 'date';
+  type: "date";
   date: Record<string, never>;
 }
 
 export interface PeopleProperty extends PropertyBase {
-  type: 'people';
+  type: "people";
   people: Record<string, never>;
 }
 
 export interface FileProperty extends PropertyBase {
-  type: 'file';
+  type: "file";
   file: Record<string, never>;
 }
 
 export interface CheckboxProperty extends PropertyBase {
-  type: 'checkbox';
+  type: "checkbox";
   checkbox: Record<string, never>;
 }
 
 export interface URLProperty extends PropertyBase {
-  type: 'url';
+  type: "url";
   url: Record<string, never>;
 }
 
 export interface EmailProperty extends PropertyBase {
-  type: 'email';
+  type: "email";
   email: Record<string, never>;
 }
 
 export interface PhoneNumberProperty extends PropertyBase {
-  type: 'phone_number';
+  type: "phone_number";
   phone_number: Record<string, never>;
 }
 
 export interface FormulaProperty extends PropertyBase {
-  type: 'formula';
+  type: "formula";
   formula: {
     expression: string;
   };
 }
 
 export interface RelationProperty extends PropertyBase {
-  type: 'relation';
+  type: "relation";
   relation: {
     database_id: string;
     synced_property_name?: string;
     synced_property_id?: string;
-  }
+  };
 }
 
 export interface RollupProperty extends PropertyBase {
-  type: 'rollup';
+  type: "rollup";
   rollup: {
     relation_property_name: string;
     relation_property_id: string;
     rollup_property_name: string;
     rollup_property_id: string;
-    function: 'count_all' | 'count_values' | 'count_unique_values' | 'count_empty' | 'count_not_empty' | 'percent_empty' | 'percent_not_empty' | 'sum' | 'average' | 'median' | 'min' | 'max' | 'range';
-  }
+    function:
+      | "count_all"
+      | "count_values"
+      | "count_unique_values"
+      | "count_empty"
+      | "count_not_empty"
+      | "percent_empty"
+      | "percent_not_empty"
+      | "sum"
+      | "average"
+      | "median"
+      | "min"
+      | "max"
+      | "range";
+  };
 }
 
 export interface CreatedTimeProperty extends PropertyBase {
-  type: 'created_time';
+  type: "created_time";
   created_time: Record<string, never>;
 }
 
 export interface CreatedByProperty extends PropertyBase {
-  type: 'created_by';
+  type: "created_by";
   created_by: Record<string, never>;
 }
 
 export interface LastEditedTimeProperty extends PropertyBase {
-  type: 'last_edited_time';
+  type: "last_edited_time";
   last_edited_time: Record<string, never>;
 }
 
 export interface LastEditedByProperty extends PropertyBase {
-  type: 'last_edited_by';
+  type: "last_edited_by";
   last_edited_by: Record<string, never>;
 }
 
@@ -274,7 +298,7 @@ export interface LastEditedByProperty extends PropertyBase {
 export type User = PersonUser | BotUser;
 
 export interface UserBase {
-  object: 'user';
+  object: "user";
   id: string;
   type?: string;
   name?: string;
@@ -282,16 +306,15 @@ export interface UserBase {
 }
 
 export interface PersonUser extends UserBase {
-  type?: 'person';
+  type?: "person";
   person?: {
     email: string;
   };
 }
 
 export interface BotUser extends UserBase {
-  type?: 'bot';
+  type?: "bot";
 }
-
 
 /*
  * Misc (output)
@@ -309,19 +332,37 @@ export interface MultiSelectOption {
   color: Color;
 }
 
-
 export interface SearchSort {
-  direction: 'ascending' | 'descending';
-  timestamp: 'last_edited_time';
+  direction: "ascending" | "descending";
+  timestamp: "last_edited_time";
 }
 
 export interface SearchFilter {
-  value: 'object',
-  property: 'object';
+  value: "object";
+  property: "object";
 }
 
-export type Color = 'default' | 'gray' | 'brown' | 'orange' | 'yellow' | 'green' | 'blue' | 'purple' | 'pink' | 'red';
-export type BackgroundColor = 'gray_background' | 'brown_background' | 'orange_background' | 'yellow_background' | 'green_background' | 'blue_background' | 'purple_background' | 'pink_background' | 'red_background';
+export type Color =
+  | "default"
+  | "gray"
+  | "brown"
+  | "orange"
+  | "yellow"
+  | "green"
+  | "blue"
+  | "purple"
+  | "pink"
+  | "red";
+export type BackgroundColor =
+  | "gray_background"
+  | "brown_background"
+  | "orange_background"
+  | "yellow_background"
+  | "green_background"
+  | "blue_background"
+  | "purple_background"
+  | "pink_background"
+  | "red_background";
 
 /*
  * Filter (input)
@@ -339,7 +380,7 @@ export type SinglePropertyFilter =
   | PeopleFilter
   | FilesFilter
   | RelationFilter
-  | FormulaFilter
+  | FormulaFilter;
 
 export interface CompoundFilter {
   or?: Filter[];
@@ -427,10 +468,10 @@ export interface RelationFilter extends SinglePropertyFilterBase {
 }
 
 export interface FormulaFilter extends SinglePropertyFilterBase {
-  text?: Omit<TextFilter, 'property'>;
-  checkbox?: Omit<CheckboxFilter, 'property'>;
-  number?: Omit<NumberFilter, 'property'>;
-  date?: Omit<DateFilter, 'property'>;
+  text?: Omit<TextFilter, "property">;
+  checkbox?: Omit<CheckboxFilter, "property">;
+  number?: Omit<NumberFilter, "property">;
+  date?: Omit<DateFilter, "property">;
 }
 
 /*
@@ -439,8 +480,8 @@ export interface FormulaFilter extends SinglePropertyFilterBase {
 
 export interface Sort {
   property?: string;
-  timestamp?: 'created_time' | 'last_edited_time';
-  direction: 'ascending' | 'descending';
+  timestamp?: "created_time" | "last_edited_time";
+  direction: "ascending" | "descending";
 }
 
 /*
@@ -448,7 +489,7 @@ export interface Sort {
  */
 
 export interface Page {
-  object: 'page',
+  object: "page";
   id: string;
   parent: Parent;
   properties: { [propertyName: string]: PropertyValue };
@@ -459,23 +500,24 @@ export interface Page {
  */
 
 export type Parent = DatabaseParent | PageParent | WorkspaceParent;
-export type ParentInput = Omit<DatabaseParent, 'type'> |  Omit<PageParent, 'type'>;
+export type ParentInput =
+  | Omit<DatabaseParent, "type">
+  | Omit<PageParent, "type">;
 // TODO: use distributiveomit?
 
 export interface DatabaseParent {
-  type: 'database_id';
+  type: "database_id";
   database_id: string;
 }
 
 export interface PageParent {
-  type: 'page_id';
+  type: "page_id";
   page_id: string;
 }
 
 export interface WorkspaceParent {
-  type: 'workspace';
+  type: "workspace";
 }
-
 
 /*
  * PropertyValue
@@ -487,24 +529,24 @@ export interface WorkspaceParent {
 //     : never;
 
 export type PropertyValue =
- | TitlePropertyValue
- | RichTextPropertyValue
- | NumberPropertyValue
- | SelectPropertyValue
- | MultiSelectPropertyValue
- | DatePropertyValue
- | FormulaPropertyValue
- | RollupPropertyValue
- | PeoplePropertyValue
- | FilesPropertyValue
- | CheckboxPropertyValue
- | URLPropertyValue
- | EmailPropertyValue
- | PhoneNumberPropertyValue
- | CreatedTimePropertyValue
- | CreatedByPropertyValue
- | LastEditedTimePropertyValue
- | LastEditedByPropertyValue;
+  | TitlePropertyValue
+  | RichTextPropertyValue
+  | NumberPropertyValue
+  | SelectPropertyValue
+  | MultiSelectPropertyValue
+  | DatePropertyValue
+  | FormulaPropertyValue
+  | RollupPropertyValue
+  | PeoplePropertyValue
+  | FilesPropertyValue
+  | CheckboxPropertyValue
+  | URLPropertyValue
+  | EmailPropertyValue
+  | PhoneNumberPropertyValue
+  | CreatedTimePropertyValue
+  | CreatedByPropertyValue
+  | LastEditedTimePropertyValue
+  | LastEditedByPropertyValue;
 
 export interface PropertyValueBase {
   id: string;
@@ -512,22 +554,22 @@ export interface PropertyValueBase {
 }
 
 export interface TitlePropertyValue extends PropertyValueBase {
-  type: 'title';
+  type: "title";
   title: RichText[];
 }
 
 export interface RichTextPropertyValue extends PropertyValueBase {
-  type: 'rich_text';
+  type: "rich_text";
   rich_text: RichText[];
 }
 
 export interface NumberPropertyValue extends PropertyValueBase {
-  type: 'number';
+  type: "number";
   number: number;
 }
 
 export interface SelectPropertyValue extends PropertyValueBase {
-  type: 'select';
+  type: "select";
   select: {
     id: string;
     name: string;
@@ -536,7 +578,7 @@ export interface SelectPropertyValue extends PropertyValueBase {
 }
 
 export interface MultiSelectPropertyValue extends PropertyValueBase {
-  type: 'multi_select';
+  type: "multi_select";
   multi_select: {
     id: string;
     name: string;
@@ -545,7 +587,7 @@ export interface MultiSelectPropertyValue extends PropertyValueBase {
 }
 
 export interface DatePropertyValue extends PropertyValueBase {
-  type: 'date';
+  type: "date";
   date: {
     start: string;
     end?: string;
@@ -553,92 +595,96 @@ export interface DatePropertyValue extends PropertyValueBase {
 }
 
 export interface FormulaPropertyValue extends PropertyValueBase {
-  type: 'formula';
-  formula: StringFormulaValue | NumberFormulaValue | BooleanFormulaValue | DateFormulaValue;
+  type: "formula";
+  formula:
+    | StringFormulaValue
+    | NumberFormulaValue
+    | BooleanFormulaValue
+    | DateFormulaValue;
 }
 
 export interface StringFormulaValue {
-  type: 'string';
+  type: "string";
   string?: string;
 }
 export interface NumberFormulaValue {
-  type: 'number';
+  type: "number";
   number?: number;
 }
 export interface BooleanFormulaValue {
-  type: 'boolean';
+  type: "boolean";
   boolean: boolean;
 }
 export interface DateFormulaValue {
-  type: 'date';
+  type: "date";
   date: DatePropertyValue;
 }
 
 export interface RollupPropertyValue extends PropertyValueBase {
-  type: 'rollup';
+  type: "rollup";
   rollup: NumberRollupValue | DateRollupValue | ArrayRollupValue;
 }
 
 export interface NumberRollupValue {
-  type: 'number';
+  type: "number";
   number: number;
 }
 export interface DateRollupValue {
-  type: 'date';
+  type: "date";
   date: DatePropertyValue;
 }
 export interface ArrayRollupValue {
-  type: 'array';
+  type: "array";
   array: unknown[]; // TODO: this is exactly like PropertyValue but without the `id`
 }
 
 export interface PeoplePropertyValue extends PropertyValueBase {
-  type: 'people';
+  type: "people";
   people: User[];
 }
 
 export interface FilesPropertyValue extends PropertyValueBase {
-  type: 'files';
-  files: { name: string; }[];
+  type: "files";
+  files: { name: string }[];
 }
 
 export interface CheckboxPropertyValue extends PropertyValueBase {
-  type: 'checkbox';
+  type: "checkbox";
   checkbox: boolean;
 }
 
 export interface URLPropertyValue extends PropertyValueBase {
-  type: 'url';
+  type: "url";
   url: string;
 }
 
 export interface EmailPropertyValue extends PropertyValueBase {
-  type: 'email';
+  type: "email";
   email: string;
 }
 
 export interface PhoneNumberPropertyValue extends PropertyValueBase {
-  type: 'phone_number';
+  type: "phone_number";
   phone_number: string;
 }
 
 export interface CreatedTimePropertyValue extends PropertyValueBase {
-  type: 'created_time';
+  type: "created_time";
   created_time: string;
 }
 
 export interface CreatedByPropertyValue extends PropertyValueBase {
-  type: 'created_by';
+  type: "created_by";
   created_by: User;
 }
 
 export interface LastEditedTimePropertyValue extends PropertyValueBase {
-  type: 'last_edited_time';
+  type: "last_edited_time";
   last_edited_time: string;
 }
 
 export interface LastEditedByPropertyValue extends PropertyValueBase {
-  type: 'last_edited_by';
+  type: "last_edited_by";
   last_edited_by: User;
 }
 
@@ -655,40 +701,40 @@ export interface RichTextBase {
 }
 
 export interface RichTextText extends RichTextBase {
-  type: 'text';
+  type: "text";
   text: {
     content: string;
-    link?: { type: 'url'; url: string; };
+    link?: { type: "url"; url: string };
   };
 }
 
 export interface RichTextMention extends RichTextBase {
-  type: 'mention';
+  type: "mention";
   mention: UserMention | PageMention | DatabaseMention | DateMention;
 }
 
 export interface UserMention {
-  type: 'user';
+  type: "user";
   user: User;
 }
 
 export interface PageMention {
-  type: 'page';
-  page: { id: string; };
+  type: "page";
+  page: { id: string };
 }
 
 export interface DatabaseMention {
-  type: 'database';
+  type: "database";
   database: { id: string };
 }
 
 export interface DateMention {
-  type: 'date';
+  type: "date";
   date: DatePropertyValue;
 }
 
 export interface RichTextEquation extends RichTextBase {
-  type: 'equation';
+  type: "equation";
   equation: {
     expression: string;
   };
