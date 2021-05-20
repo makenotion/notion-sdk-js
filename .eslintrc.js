bb9d6fc49db407a74edf1f4558dbdda8e90b1b35
Jake Teton-Landis <just.1.jake@gmail.com>
2021-05-20 09:02:03 -0700

    apply prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,13 +2,11 @@
 
 module.exports = {
   root: true,
-  parser: '@typescript-eslint/parser',
-  plugins: [
-    '@typescript-eslint'
-  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended'
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
   ],
-};
+}
--- src/logging.ts
+++ src/logging.ts
@@ -1,20 +1,20 @@
-import { assertNever } from './helpers';
+import { assertNever } from "./helpers"
 
 export enum LogLevel {
-  DEBUG = 'debug',
-  INFO = 'info',
-  WARN = 'warn',
-  ERROR = 'error',
+  DEBUG = "debug",
+  INFO = "info",
+  WARN = "warn",
+  ERROR = "error",
 }
 
 export interface Logger {
-  (level: LogLevel, message: string, extraInfo: Record<string, unknown>): void;
+  (level: LogLevel, message: string, extraInfo: Record<string, unknown>): void
 }
 
 export function makeConsoleLogger(name: string): Logger {
   return (level, message, extraInfo) => {
-    console[level](`${name} ${level}:`, message, extraInfo);
-  };
+    console[level](`${name} ${level}:`, message, extraInfo)
+  }
 }
 
 /**
@@ -22,10 +22,15 @@ export function makeConsoleLogger(name: string): Logger {
  */
 export function logLevelSeverity(level: LogLevel): number {
   switch (level) {
-    case LogLevel.DEBUG: return 20;
-    case LogLevel.INFO: return 40;
-    case LogLevel.WARN: return 60;
-    case LogLevel.ERROR: return 80;
-    default: return assertNever(level);
+    case LogLevel.DEBUG:
+      return 20
+    case LogLevel.INFO:
+      return 40
+    case LogLevel.WARN:
+      return 60
+    case LogLevel.ERROR:
+      return 80
+    default:
+      return assertNever(level)
   }
 }
