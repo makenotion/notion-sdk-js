b00b05c34fac89fd1b90c79292b2dfbd222651e5
chaz-kiker <chaz-kiker@lambdastudents.com>
2021-07-13 15:45:26 -0500

    add more verbose comments in 'notion-github-sync' example; explain the recursive async fn better

--- examples/notion-github-sync/index.js
+++ examples/notion-github-sync/index.js
@@ -19,38 +19,63 @@ const database_id = process.env.NOTION_DATABASE_ID
 
 async function syncIssuesWithDatabase() {
   console.log("Syncing GitHub Issues with Notion Database")
+
+  // retrieve all of the issues from the current Notion page
   const issues_in_db = await getIssuesFromDatabase()
 
-  // Get a list of github issues and add them to a local store
+  //
+  // Second: Get a list of github issues and add them to a local store
+  //
+
+  // `github_issues` will have (key, value) entries such that
+  // key = issue number
+  // value = an object with the relevant fields of the issue `{id, title, state, comments}`
   let github_issues = {}
+
+  // make an async iterator of paginated data from GitHub: each page of data will hold a list of issues
   const iterator = octokit.paginate.iterator(octokit.rest.issues.listForRepo, {
     owner: process.env.GITHUB_REPO_OWNER,
     repo: process.env.GITHUB_REPO_NAME,
     per_page: 100,
   })
 
-  for await (const { data: issues } of iterator) {
-    for (const issue of issues) {
-      github_issues[issue.number] = {
-        id: issue.id,
-        title: issue.title,
-        state: issue.state,
-        comments: issue.comments,
+  // for every page in the iterator,
+  for await (const page of iterator) {
+    //`page.data` holds the list of issues.
+    // Iterate through every issue in the current list of issues
+    for (const issue of page.data) {
+      // de-structure the relevant properties from our issue
+      const {id, title, state, comments, number} = issue;
+      // and add all those properties at the given key `issue.number` in our `github_issues` dictionary
+      github_issues[number] = {
+        id,
+        title,
+        state,
+        comments,
       }
     }
   }
 
+  //
+  // Now that we have all of the relevant data, we need to update the Notion Database.
+  // If the issue already exists in the DB, update it.
+  // If the issue doesn't exist yet, create it.
+  //
+
   // Create new issues or update existing in a Notion Database
-  for (const [issue_number, issues_details] of Object.entries(github_issues)) {
+  for (const [key, value] of Object.entries(github_issues)) {
+    const issue_number = key
+    const issue_details = value
+
     // If the issue does not exist in the database yet, add it to the database
     if (!(issue_number in issues_in_db)) {
       await notion.pages.create({
         parent: { database_id },
         properties: {
-          State: { name: issues_details.state },
+          State: { name: issue_details.state },
           "Issue Number": parseInt(issue_number),
-          Name: [{ text: { content: issues_details.title } }],
-          Comments: parseInt(issues_details.comments),
+          Name: [{ text: { content: issue_details.title } }],
+          Comments: parseInt(issue_details.comments),
         },
       })
     }
@@ -59,10 +84,10 @@ async function syncIssuesWithDatabase() {
       await notion.pages.update({
         page_id: issues_in_db[issue_number].page_id,
         properties: {
-          State: { name: issues_details.state },
+          State: { name: issue_details.state },
           "Issue Number": parseInt(issue_number),
-          Name: [{ text: { content: issues_details.title } }],
-          Comments: parseInt(issues_details.comments),
+          Name: [{ text: { content: issue_details.title } }],
+          Comments: parseInt(issue_details.comments),
         },
       })
     }
@@ -71,25 +96,39 @@ async function syncIssuesWithDatabase() {
   setTimeout(syncIssuesWithDatabase, 5 * 60 * 1000)
 }
 
-// Get a paginated list of Tasks currently in a the database.
+// returns a dictionary with (key, value) entries such that
+// key = issue_number
+// value = an object containing the `page_id` of that specific issue in the database
 async function getIssuesFromDatabase() {
   const issues = {}
 
-  async function getPageOfIssues(cursor) {
+  // async loop -- while there are issues in our database,
+  // update the `issues` object at each key (issue number) to hold the proper page id
+  async function getPageOfData(cursor) {
     const db = await notion.databases.query({
       database_id,
+      // if method was called without passing in `cursor`, this `start_cursor` is simply `undefined`!
       start_cursor: cursor,
     })
+
+    // for every page of data in the results
     for (const page of db.results) {
+      // update `issues` at the current
       issues[page.properties["Issue Number"].number] = {
         page_id: page.id,
       }
     }
+
+    // if there are more pages to query
     if (db.has_more) {
-      await getPageOfIssues(db.next_cursor)
+      // recurse to continue with the next paginated list
+      await getPageOfData(db.next_cursor)
     }
   }
-  await getPageOfIssues()
+
+  // calling this here kicks off the first iteration of our async loop
+  await getPageOfData();
+
   return issues
 }
 
