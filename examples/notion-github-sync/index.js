ba0e53eab299fa14fbd093012fe2de0e116e6d64
Lauren Oliveri <lauren@makenotion.com>
2021-07-21 01:50:02 -0400

    fix github sync example

--- examples/notion-github-sync/index.js
+++ examples/notion-github-sync/index.js
@@ -7,116 +7,228 @@
 
 ================================================================================ */
 
-import { Client } from "@notionhq/client"
-import dotenv from "dotenv"
-import { Octokit } from "octokit"
+const { Client } = require("@notionhq/client")
+const dotenv = require("dotenv")
+const { Octokit } = require("octokit")
 
 dotenv.config()
 const octokit = new Octokit({ auth: process.env.GITHUB_KEY })
 const notion = new Client({ auth: process.env.NOTION_KEY })
 
-const database_id = process.env.NOTION_DATABASE_ID
+const databaseId = process.env.NOTION_DATABASE_ID
+const OPERATION_BATCH_SIZE = 10
 
-async function syncIssuesWithDatabase() {
-  console.log("Syncing GitHub Issues with Notion Database")
-  const issuesInDatabase = await getIssuesFromDatabase()
+/**
+ * Local map to store  GitHub issue ID to its Notion pageId.
+ * { [issueId: string]: string }
+ */
+const gitHubIssuesIdToNotionPageId = {}
 
-  // Get a list of github issues and add them to a local store.
-  let gitHubIssues = {}
+/**
+ * Initialize local data store.
+ * Then sync with GitHub.
+ */
+setInitialGitHubToNotionIdMap().then(syncNotionDatabaseWithGitHub)
+
+/**
+ * Get and set the initial data store with issues currently in the database.
+ */
+async function setInitialGitHubToNotionIdMap() {
+  const currentIssues = await getIssuesFromNotionDatabase()
+  for (const { pageId, issueNumber } of currentIssues) {
+    gitHubIssuesIdToNotionPageId[issueNumber] = pageId
+  }
+}
+
+async function syncNotionDatabaseWithGitHub() {
+  // Get all issues currently in the provided GitHub repository.
+  console.log("\nFetching issues from Notion DB...")
+  const issues = await getGitHubIssuesForRepository()
+  console.log(`Fetched ${issues.length} issues from GitHub repository.`)
+  // Group issues into those that need to be created or updated in the Notion database.
+  const { pagesToCreate, pagesToUpdate } = getNotionOperations(issues)
+  // Create pages for new issues.
+  console.log(`\n${pagesToCreate.length} new issues to add to Notion.`)
+  await createPages(pagesToCreate)
+  // Updates pages for existing issues.
+  console.log(`\n${pagesToUpdate.length} issues to update in Notion.`)
+  await updatePages(pagesToUpdate)
+  // Success!
+  console.log("\n✅ Notion database is synced with GitHub.")
+}
+
+/**
+ * Gets issues from the database.
+ *
+ * Returns array of objects with pageId and issueNumber
+ * Array<{ pageId: string, issueNumber: number }>
+ */
+async function getIssuesFromNotionDatabase() {
+  const pages = []
+  let cursor = undefined
+  while (true) {
+    console.log("\nFetching issues from Notion DB...")
+    const { results, next_cursor } = await notion.databases.query({
+      database_id: databaseId,
+      start_cursor: cursor,
+    })
+    pages.push(...results)
+    if (!next_cursor) {
+      break
+    }
+    cursor = next_cursor
+  }
+  console.log(`${pages.length} issues successfully fetched.`)
+  return pages.map(page => {
+    return {
+      pageId: page.id,
+      issueNumber: page.properties["Issue Number"].number,
+    }
+  })
+}
+
+/**
+ * Gets issues from a GitHub repository. Pull requests are omitted.
+ *
+ * Returns array of issue objects with number, title, state, comment_count, and url.
+ * Array<{ number: number, title: string, state: "open" | "closed", comment_count: number, url: string }>
+ */
+async function getGitHubIssuesForRepository() {
+  const issues = []
+  // https://docs.github.com/en/rest/guides/traversing-with-pagination
+  // https://docs.github.com/en/rest/reference/issues
   const iterator = octokit.paginate.iterator(octokit.rest.issues.listForRepo, {
     owner: process.env.GITHUB_REPO_OWNER,
     repo: process.env.GITHUB_REPO_NAME,
+    state: "all",
     per_page: 100,
   })
-
-  for await (const { data: issues } of iterator) {
-    for (const issue of issues) {
-      gitHubIssues[issue.number] = {
-        id: issue.id,
-        title: issue.title,
-        state: issue.state,
-        comments: issue.comments,
+  for await (const { data } of iterator) {
+    for (const issue of data) {
+      if (!issue.pull_request) {
+        issues.push({
+          number: issue.number,
+          title: issue.title,
+          state: issue.state,
+          comment_count: issue.comments,
+          url: issue.html_url,
+        })
       }
     }
   }
-
-  // Create new issues or update existing in a Notion Database.
-  for (const [key, value] of Object.entries(gitHubIssues)) {
-    const issue_number = key
-    const issues_details = value
-    // If the issue does not exist in the database yet, add it to the database.
-    if (!(issue_number in issuesInDatabase)) {
-      await notion.request({
-        path: "pages",
-        method: "POST",
-        body: {
-          parent: { database_id: database_id },
-          properties: {
-            State: { name: issues_details.state },
-            "Issue Number": parseInt(issue_number),
-            Name: [{ text: { content: issues_details.title } }],
-            Comments: parseInt(issues_details.comments),
-          },
-        },
-      })
-    }
-    // This issue already exists in the database so we want to update the page.
-    else {
-      await notion.request({
-        path: "pages/" + issuesInDatabase[issue_number].page_id,
-        method: "patch",
-        body: {
-          properties: {
-            State: { name: issues_details.state },
-            "Issue Number": parseInt(issue_number),
-            Name: [{ text: { content: issues_details.title } }],
-            Comments: parseInt(issues_details.comments),
-          },
-        },
-      })
-    }
-  }
-  // Run this function every five minutes.
-  setTimeout(syncIssuesWithDatabase, 5 * 60 * 1000)
+  return issues
 }
 
-;(async () => {
-  syncIssuesWithDatabase()
-})()
-
-// Get a paginated list of Tasks currently in a the database.
-async function getIssuesFromDatabase() {
-  const issues = {}
-
-  async function getPageOfIssues(cursor) {
-    let request_payload = ""
-    // Create the request payload based on the presence of a start_cursor.
-    if (cursor == undefined) {
-      request_payload = {
-        path: "databases/" + database_id + "/query",
-        method: "POST",
-      }
+/**
+ * Determines which issues already exist in the Notion database.
+ *
+ * @param issues Array<{ number: number, title: string, state: "open" | "closed", comment_count: number, url: string }>
+ *
+ * Returns an object with keys for each operation grouping:
+ * {
+ *  pagesToCreate: Array<{ number: number, title: string, state: "open" | "closed", comment_count: number, url: string }>
+ *  pagesToUpdate: Array<{ pageId: string, number: number, title: string, state: "open" | "closed", comment_count: number, url: string }>
+ * }
+ */
+function getNotionOperations(issues) {
+  const pagesToCreate = []
+  const pagesToUpdate = []
+  for (const issue of issues) {
+    const pageId = gitHubIssuesIdToNotionPageId[issue.number]
+    if (pageId) {
+      pagesToUpdate.push({
+        ...issue,
+        pageId,
+      })
     } else {
-      request_payload = {
-        path: "databases/" + database_id + "/query",
-        method: "POST",
-        body: {
-          start_cursor: cursor,
-        },
-      }
-    }
-    // While there are more pages left in the query, get pages from the database.
-    const current_pages = await notion.request(request_payload)
-
-    for (const page of current_pages.results) {
-      issues[page.properties["Issue Number"].number] = {
-        page_id: page.id,
-      }
-    }
-    if (current_pages.has_more) {
-      await getPageOfIssues(current_pages.next_cursor)
+      pagesToCreate.push(issue)
     }
   }
-  await getPageOfIssues()
-  return issues
+  return { pagesToCreate, pagesToUpdate }
+}
+
+/**
+ * Creates new pages in Notion.
+ *
+ * @param  Array<{ number: number, title: string, state: "open" | "closed", comment_count: number, url: string }>
+ */
+async function createPages(pagesToCreate) {
+  for (const pagesToCreateBatch of chunkItems(pagesToCreate)) {
+    await Promise.all(
+      pagesToCreateBatch.map(issue =>
+        notion.pages.create({
+          parent: { database_id: databaseId },
+          properties: getPropertiesFromIssue(issue),
+        })
+      )
+    )
+    console.log(`Completed batch size: ${pagesToCreateBatch.length}`)
+  }
+}
+
+/**
+ * Updates provided pages in Notion.
+ *
+ * @param  Array<{ pageId: string, number: number, title: string, state: "open" | "closed", comment_count: number, url: string }>
+ */
+async function updatePages(pagesToUpdate) {
+  // https://developers.notion.com/reference/patch-page
+  for (const pagesToUpdateBatch of chunkItems(pagesToUpdate)) {
+    await Promise.all(
+      pagesToUpdateBatch.map(({ pageId, ...issue }) =>
+        notion.pages.update({
+          page_id: pageId,
+          properties: getPropertiesFromIssue(issue),
+        })
+      )
+    )
+    console.log(`Completed batch size: ${pagesToUpdateBatch.length}`)
+  }
+}
+
+//*========================================================================
+// Helpers
+//*========================================================================
+
+/**
+ * An iterator that batches out a list of items into smaller chunks.
+ */
+function* chunkItems(items) {
+  let startIndex = 0
+  while (true) {
+    const itemChunk = items.slice(startIndex, startIndex + OPERATION_BATCH_SIZE)
+    yield itemChunk
+    if (itemChunk.length < OPERATION_BATCH_SIZE) {
+      break
+    }
+    startIndex += OPERATION_BATCH_SIZE
+  }
+}
+
+/**
+ * Formats a GitHub issue to match Notion database fields.
+ *
+ * @param issue { number: number, title: string, state: "open" | "closed", comment_count: number, url: string }
+ *
+ * Returns a Notion database request property object.
+ */
+function getPropertiesFromIssue(issue) {
+  const { title, number, state, comment_count, url } = issue
+  return {
+    Name: {
+      title: [{ type: "text", text: { content: title } }],
+    },
+    "Issue Number": {
+      number,
+    },
+    State: {
+      select: { name: state },
+    },
+    "Number of Comments": {
+      number: comment_count,
+    },
+    "Issue URL": {
+      url,
+    },
+  }
 }
