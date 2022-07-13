4b292a72ecebb2eeaa5a224882fc63876c4536bd
Bill Cauchois <bill@makenotion.com>
2022-07-13 12:05:30 -0400

    Add doc comments for helpers

--- src/helpers.ts
+++ src/helpers.ts
@@ -20,6 +20,25 @@ interface PaginatedList<T> {
   has_more: boolean
 }
 
+/**
+ * Returns an async iterator over the results of any paginated Notion API.
+ *
+ * Example (given a notion Client called `notion`):
+ *
+ * ```
+ * for await (const block of iteratePaginatedAPI(notion.blocks.children.list, {
+ *   block_id: parentBlockId,
+ * })) {
+ *   // Do something with block.
+ * }
+ * ```
+ *
+ * @param listFn A bound function on the Notion client that represents a conforming paginated
+ *   API. Example: `notion.blocks.children.list`.
+ * @param firstPageArgs Arguments that should be passed to the API on the first and subsequent
+ *   calls to the API. Any necessary `next_cursor` will be automatically populated by
+ *   this function. Example: `{ block_id: "<my block id>" }`
+ */
 export async function* iteratePaginatedAPI<Args extends PaginatedArgs, Item>(
   listFn: (args: Args) => Promise<PaginatedList<Item>>,
   firstPageArgs: Args
@@ -35,6 +54,24 @@ export async function* iteratePaginatedAPI<Args extends PaginatedArgs, Item>(
   } while (nextCursor)
 }
 
+/**
+ * Collect all of the results of paginating an API into an in-memory array.
+ *
+ * Example (given a notion Client called `notion`):
+ *
+ * ```
+ * const blocks = collectPaginatedAPI(notion.blocks.children.list, {
+ *   block_id: parentBlockId,
+ * })
+ * // Do something with blocks.
+ * ```
+ *
+ * @param listFn A bound function on the Notion client that represents a conforming paginated
+ *   API. Example: `notion.blocks.children.list`.
+ * @param firstPageArgs Arguments that should be passed to the API on the first and subsequent
+ *   calls to the API. Any necessary `next_cursor` will be automatically populated by
+ *   this function. Example: `{ block_id: "<my block id>" }`
+ */
 export async function collectPaginatedAPI<Args extends PaginatedArgs, Item>(
   listFn: (args: Args) => Promise<PaginatedList<Item>>,
   firstPageArgs: Args
@@ -46,24 +83,36 @@ export async function collectPaginatedAPI<Args extends PaginatedArgs, Item>(
   return results
 }
 
+/**
+ * @returns `true` if `response` is a full `BlockObjectResponse`.
+ */
 export function isFullBlock(
   response: BlockObjectResponse | PartialBlockObjectResponse
 ): response is BlockObjectResponse {
   return "type" in response
 }
 
+/**
+ * @returns `true` if `response` is a full `PageObjectResponse`.
+ */
 export function isFullPage(
   response: PageObjectResponse | PartialPageObjectResponse
 ): response is PageObjectResponse {
   return "url" in response
 }
 
+/**
+ * @returns `true` if `response` is a full `DatabaseObjectResponse`.
+ */
 export function isFullDatabase(
   response: DatabaseObjectResponse | PartialDatabaseObjectResponse
 ): response is DatabaseObjectResponse {
   return "title" in response
 }
 
+/**
+ * @returns `true` if `response` is a full `UserObjectResponse`.
+ */
 export function isFullUser(
   response: UserObjectResponse | PartialUserObjectResponse
 ): response is UserObjectResponse {
