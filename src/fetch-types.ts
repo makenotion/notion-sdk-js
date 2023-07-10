e224f192b8a266e930cf3435016fac8bce3c1ce0
huw <yo@huw.cool>
2023-07-10 17:06:59 +0000

    Fully type the fetch function and test it against major implementations

--- src/fetch-types.ts
+++ src/fetch-types.ts
@@ -1,30 +1,30 @@
+import type { Agent } from "http"
 import type { Assert } from "./type-utils"
-import type {
-  RequestInit as NodeRequestInit,
-  Response as NodeResponse,
-} from "node-fetch"
+import type NodeFetchFn from "node-fetch"
+import type { RequestInfo as NodeFetchRequestInfo, RequestInit as NodeFetchRequestInit, Response as NodeFetchResponse } from "node-fetch"
+import type { RequestInfo as CloudflareRequestInfo, RequestInit as CloudflareRequestInit, Response as CloudflareResponse, fetch as CloudflareFetchFn } from "@cloudflare/workers-types"
 
-type FetchFn = typeof fetch
-type FetchResponse = Awaited<ReturnType<FetchFn>>
-type RequestInfo = Parameters<FetchFn>[0]
-type RequestInit = NonNullable<Parameters<FetchFn>[1]>
+type FetchFn = typeof fetch;
 
 export type SupportedRequestInfo = string
-type _assertSupportedInfoIsSubtype = Assert<RequestInfo, SupportedRequestInfo>
+type _assertSupportedInfoIsSubtypeOfNative = Assert<RequestInfo, SupportedRequestInfo>
+type _assertSupportedInfoIsSubtypeOfNodeFetch = Assert<NodeFetchRequestInfo, SupportedRequestInfo>
+type _assertSupportedInfoIsSubtypeOfCloudflare = Assert<CloudflareRequestInfo, SupportedRequestInfo>
 
-export type SupportedRequestInit = {
-  agent?: NodeRequestInit["agent"]
-  body?: NonNullable<RequestInit["body"]> & NonNullable<NodeRequestInit["body"]>
-  headers?: NonNullable<RequestInit["headers"]> &
-    NonNullable<NodeRequestInit["headers"]>
-  method?: RequestInit["method"]
-  redirect?: RequestInit["redirect"]
-}
-type _assertSupportedInitIsSubtype = Assert<RequestInit, SupportedRequestInit>
+export type SupportedRequestInit = { agent?: Agent; body?: string; headers?: Record<string, string>; method?: string }
+type _assertSupportedInitIsSubtypeOfNative = Assert<RequestInit, SupportedRequestInit>
+type _assertSupportedInitIsSubtypeOfNodeFetch = Assert<NodeFetchRequestInit, SupportedRequestInit>
+type _assertSupportedInitIsSubtypeOfCloudflare = Assert<CloudflareRequestInit, SupportedRequestInit>
 
-export type SupportedResponse = FetchResponse | NodeResponse
+export type SupportedResponse = { ok: boolean; text: () => Promise<string>; headers: unknown; status: number }
+type _assertSupportedResponseIsSubtypeOfNative = Assert<SupportedResponse, Response>
+type _assertSupportedResponseIsSubtypeOfNodeFetch = Assert<SupportedResponse, NodeFetchResponse>
+type _assertSupportedResponseIsSubtypeOfCloudflare = Assert<SupportedResponse, CloudflareResponse>
 
 export type SupportedFetch = (
   url: SupportedRequestInfo,
   init?: SupportedRequestInit
 ) => Promise<SupportedResponse>
+type _assertSupportedFetchIsSubtypeOfNative = Assert<SupportedFetch, FetchFn>
+type _assertSupportedFetchIsSubtypeOfNodeFetch = Assert<SupportedFetch, typeof NodeFetchFn>
+type _assertSupportedFetchIsSubtypeOfCloudflare = Assert<SupportedFetch, typeof CloudflareFetchFn>
