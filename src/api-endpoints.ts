1d97c189ee305da8910ac614a224825ff3d0f817
kiranpandit <kiran@makenotion.com>
2023-07-24 11:34:52 -0700

    Endpoint changes for wikis (#428)
    * Wiki related changes
    
    * Remove oauth related endpoints

--- src/api-endpoints.ts
+++ src/api-endpoints.ts
@@ -4731,9 +4731,10 @@ export type DatabaseObjectResponse = {
     | null
   properties: Record<string, DatabasePropertyConfigResponse>
   parent:
+    | { type: "database_id"; database_id: string }
     | { type: "page_id"; page_id: string }
-    | { type: "workspace"; workspace: true }
     | { type: "block_id"; block_id: string }
+    | { type: "workspace"; workspace: true }
   created_by: PartialUserObjectResponse
   last_edited_by: PartialUserObjectResponse
   is_inline: boolean
@@ -9425,236 +9426,190 @@ export const listUsers = {
   path: (): string => `users`,
 } as const
 
-type CreatePageBodyParameters =
-  | {
-      parent: { database_id: IdRequest; type?: "database_id" }
-      properties:
-        | Record<
-            string,
-            | { title: Array<RichTextItemRequest>; type?: "title" }
-            | { rich_text: Array<RichTextItemRequest>; type?: "rich_text" }
-            | { number: number | null; type?: "number" }
-            | { url: TextRequest | null; type?: "url" }
+type CreatePageBodyParameters = {
+  parent:
+    | { page_id: IdRequest; type?: "page_id" }
+    | { database_id: IdRequest; type?: "database_id" }
+  properties:
+    | Record<
+        string,
+        | { title: Array<RichTextItemRequest>; type?: "title" }
+        | { rich_text: Array<RichTextItemRequest>; type?: "rich_text" }
+        | { number: number | null; type?: "number" }
+        | { url: TextRequest | null; type?: "url" }
+        | {
+            select:
+              | { id: StringRequest; name?: StringRequest; color?: SelectColor }
+              | null
+              | { name: StringRequest; id?: StringRequest; color?: SelectColor }
+              | null
+            type?: "select"
+          }
+        | {
+            multi_select: Array<
+              | { id: StringRequest; name?: StringRequest; color?: SelectColor }
+              | { name: StringRequest; id?: StringRequest; color?: SelectColor }
+            >
+            type?: "multi_select"
+          }
+        | {
+            people: Array<
+              | { id: IdRequest }
+              | {
+                  person: { email?: string }
+                  id: IdRequest
+                  type?: "person"
+                  name?: string | null
+                  avatar_url?: string | null
+                  object?: "user"
+                }
+              | {
+                  bot:
+                    | EmptyObject
+                    | {
+                        owner:
+                          | {
+                              type: "user"
+                              user:
+                                | {
+                                    type: "person"
+                                    person: { email: string }
+                                    name: string | null
+                                    avatar_url: string | null
+                                    id: IdRequest
+                                    object: "user"
+                                  }
+                                | PartialUserObjectResponse
+                            }
+                          | { type: "workspace"; workspace: true }
+                        workspace_name: string | null
+                      }
+                  id: IdRequest
+                  type?: "bot"
+                  name?: string | null
+                  avatar_url?: string | null
+                  object?: "user"
+                }
+            >
+            type?: "people"
+          }
+        | { email: StringRequest | null; type?: "email" }
+        | { phone_number: StringRequest | null; type?: "phone_number" }
+        | { date: DateRequest | null; type?: "date" }
+        | { checkbox: boolean; type?: "checkbox" }
+        | { relation: Array<{ id: IdRequest }>; type?: "relation" }
+        | {
+            files: Array<
+              | {
+                  file: { url: string; expiry_time?: string }
+                  name: StringRequest
+                  type?: "file"
+                }
+              | {
+                  external: { url: TextRequest }
+                  name: StringRequest
+                  type?: "external"
+                }
+            >
+            type?: "files"
+          }
+        | {
+            status:
+              | { id: StringRequest; name?: StringRequest; color?: SelectColor }
+              | null
+              | { name: StringRequest; id?: StringRequest; color?: SelectColor }
+              | null
+            type?: "status"
+          }
+      >
+    | Record<
+        string,
+        | Array<RichTextItemRequest>
+        | Array<RichTextItemRequest>
+        | number
+        | null
+        | TextRequest
+        | null
+        | { id: StringRequest; name?: StringRequest; color?: SelectColor }
+        | null
+        | { name: StringRequest; id?: StringRequest; color?: SelectColor }
+        | null
+        | Array<
+            | { id: StringRequest; name?: StringRequest; color?: SelectColor }
+            | { name: StringRequest; id?: StringRequest; color?: SelectColor }
+          >
+        | Array<
+            | { id: IdRequest }
             | {
-                select:
-                  | {
-                      id: StringRequest
-                      name?: StringRequest
-                      color?: SelectColor
-                    }
-                  | null
-                  | {
-                      name: StringRequest
-                      id?: StringRequest
-                      color?: SelectColor
-                    }
-                  | null
-                type?: "select"
+                person: { email?: string }
+                id: IdRequest
+                type?: "person"
+                name?: string | null
+                avatar_url?: string | null
+                object?: "user"
               }
             | {
-                multi_select: Array<
+                bot:
+                  | EmptyObject
                   | {
-                      id: StringRequest
-                      name?: StringRequest
-                      color?: SelectColor
-                    }
-                  | {
-                      name: StringRequest
-                      id?: StringRequest
-                      color?: SelectColor
-                    }
-                >
-                type?: "multi_select"
-              }
-            | {
-                people: Array<
-                  | { id: IdRequest }
-                  | {
-                      person: { email?: string }
-                      id: IdRequest
-                      type?: "person"
-                      name?: string | null
-                      avatar_url?: string | null
-                      object?: "user"
-                    }
-                  | {
-                      bot:
-                        | EmptyObject
+                      owner:
                         | {
-                            owner:
+                            type: "user"
+                            user:
                               | {
-                                  type: "user"
-                                  user:
-                                    | {
-                                        type: "person"
-                                        person: { email: string }
-                                        name: string | null
-                                        avatar_url: string | null
-                                        id: IdRequest
-                                        object: "user"
-                                      }
-                                    | PartialUserObjectResponse
+                                  type: "person"
+                                  person: { email: string }
+                                  name: string | null
+                                  avatar_url: string | null
+                                  id: IdRequest
+                                  object: "user"
                                 }
-                              | { type: "workspace"; workspace: true }
-                            workspace_name: string | null
+                              | PartialUserObjectResponse
                           }
-                      id: IdRequest
-                      type?: "bot"
-                      name?: string | null
-                      avatar_url?: string | null
-                      object?: "user"
+                        | { type: "workspace"; workspace: true }
+                      workspace_name: string | null
                     }
-                >
-                type?: "people"
-              }
-            | { email: StringRequest | null; type?: "email" }
-            | { phone_number: StringRequest | null; type?: "phone_number" }
-            | { date: DateRequest | null; type?: "date" }
-            | { checkbox: boolean; type?: "checkbox" }
-            | { relation: Array<{ id: IdRequest }>; type?: "relation" }
-            | {
-                files: Array<
-                  | {
-                      file: { url: string; expiry_time?: string }
-                      name: StringRequest
-                      type?: "file"
-                    }
-                  | {
-                      external: { url: TextRequest }
-                      name: StringRequest
-                      type?: "external"
-                    }
-                >
-                type?: "files"
-              }
-            | {
-                status:
-                  | {
-                      id: StringRequest
-                      name?: StringRequest
-                      color?: SelectColor
-                    }
-                  | null
-                  | {
-                      name: StringRequest
-                      id?: StringRequest
-                      color?: SelectColor
-                    }
-                  | null
-                type?: "status"
+                id: IdRequest
+                type?: "bot"
+                name?: string | null
+                avatar_url?: string | null
+                object?: "user"
               }
           >
-        | Record<
-            string,
-            | Array<RichTextItemRequest>
-            | Array<RichTextItemRequest>
-            | number
-            | null
-            | TextRequest
-            | null
-            | { id: StringRequest; name?: StringRequest; color?: SelectColor }
-            | null
-            | { name: StringRequest; id?: StringRequest; color?: SelectColor }
-            | null
-            | Array<
-                | {
-                    id: StringRequest
-                    name?: StringRequest
-                    color?: SelectColor
-                  }
-                | {
-                    name: StringRequest
-                    id?: StringRequest
-                    color?: SelectColor
-                  }
-              >
-            | Array<
-                | { id: IdRequest }
-                | {
-                    person: { email?: string }
-                    id: IdRequest
-                    type?: "person"
-                    name?: string | null
-                    avatar_url?: string | null
-                    object?: "user"
-                  }
-                | {
-                    bot:
-                      | EmptyObject
-                      | {
-                          owner:
-                            | {
-                                type: "user"
-                                user:
-                                  | {
-                                      type: "person"
-                                      person: { email: string }
-                                      name: string | null
-                                      avatar_url: string | null
-                                      id: IdRequest
-                                      object: "user"
-                                    }
-                                  | PartialUserObjectResponse
-                              }
-                            | { type: "workspace"; workspace: true }
-                          workspace_name: string | null
-                        }
-                    id: IdRequest
-                    type?: "bot"
-                    name?: string | null
-                    avatar_url?: string | null
-                    object?: "user"
-                  }
-              >
-            | StringRequest
-            | null
-            | StringRequest
-            | null
-            | DateRequest
-            | null
-            | boolean
-            | Array<{ id: IdRequest }>
-            | Array<
-                | {
-                    file: { url: string; expiry_time?: string }
-                    name: StringRequest
-                    type?: "file"
-                  }
-                | {
-                    external: { url: TextRequest }
-                    name: StringRequest
-                    type?: "external"
-                  }
-              >
-            | { id: StringRequest; name?: StringRequest; color?: SelectColor }
-            | null
-            | { name: StringRequest; id?: StringRequest; color?: SelectColor }
-            | null
+        | StringRequest
+        | null
+        | StringRequest
+        | null
+        | DateRequest
+        | null
+        | boolean
+        | Array<{ id: IdRequest }>
+        | Array<
+            | {
+                file: { url: string; expiry_time?: string }
+                name: StringRequest
+                type?: "file"
+              }
+            | {
+                external: { url: TextRequest }
+                name: StringRequest
+                type?: "external"
+              }
           >
-      icon?:
-        | { emoji: EmojiRequest; type?: "emoji" }
+        | { id: StringRequest; name?: StringRequest; color?: SelectColor }
         | null
-        | { external: { url: TextRequest }; type?: "external" }
+        | { name: StringRequest; id?: StringRequest; color?: SelectColor }
         | null
-      cover?: { external: { url: TextRequest }; type?: "external" } | null
-      content?: Array<BlockObjectRequest>
-      children?: Array<BlockObjectRequest>
-    }
-  | {
-      parent: { page_id: IdRequest; type?: "page_id" }
-      properties: {
-        title?:
-          | { title: Array<RichTextItemRequest>; type?: "title" }
-          | Array<RichTextItemRequest>
-      }
-      icon?:
-        | { emoji: EmojiRequest; type?: "emoji" }
-        | null
-        | { external: { url: TextRequest }; type?: "external" }
-        | null
-      cover?: { external: { url: TextRequest }; type?: "external" } | null
-      children?: Array<BlockObjectRequest>
-    }
+      >
+  icon?:
+    | { emoji: EmojiRequest; type?: "emoji" }
+    | null
+    | { external: { url: TextRequest }; type?: "external" }
+    | null
+  cover?: { external: { url: TextRequest }; type?: "external" } | null
+  content?: Array<BlockObjectRequest>
+  children?: Array<BlockObjectRequest>
+}
 
 export type CreatePageParameters = CreatePageBodyParameters
 
@@ -10292,8 +10247,6 @@ type UpdateDatabaseBodyParameters = {
         name?: string
       }
     | null
-    | { status: EmptyObject; type?: "status"; name?: string }
-    | null
     | {
         relation:
           | {
@@ -10453,12 +10406,17 @@ export type QueryDatabaseParameters = QueryDatabasePathParameters &
   QueryDatabaseBodyParameters
 
 export type QueryDatabaseResponse = {
-  type: "page"
-  page: EmptyObject
+  type: "page_or_database"
+  page_or_database: EmptyObject
   object: "list"
   next_cursor: string | null
   has_more: boolean
-  results: Array<PageObjectResponse | PartialPageObjectResponse>
+  results: Array<
+    | PageObjectResponse
+    | PartialPageObjectResponse
+    | PartialDatabaseObjectResponse
+    | DatabaseObjectResponse
+  >
 }
 
 export const queryDatabase = {
@@ -10495,7 +10453,9 @@ export const listDatabases = {
 } as const
 
 type CreateDatabaseBodyParameters = {
-  parent: { page_id: IdRequest; type?: "page_id" }
+  parent:
+    | { page_id: IdRequest; type?: "page_id" }
+    | { database_id: IdRequest; type?: "database_id" }
   properties: Record<
     string,
     | { number: { format?: NumberFormat }; type?: "number" }
@@ -10512,7 +10472,6 @@ type CreateDatabaseBodyParameters = {
         }
         type?: "multi_select"
       }
-    | { status: EmptyObject; type?: "status" }
     | {
         relation:
           | {
