36328d258d71e30cf886dbdcfaf11ba8d3bb96fb
Lucas Willems <lcswillems@users.noreply.github.com>
2023-09-30 13:49:41 +0200

    Remove node-fetch from Client.ts

--- src/Client.ts
+++ src/Client.ts
@@ -77,12 +77,10 @@ import {
   OauthTokenParameters,
   oauthToken,
 } from "./api-endpoints"
-import nodeFetch from "node-fetch"
 import {
   version as PACKAGE_VERSION,
   name as PACKAGE_NAME,
 } from "../package.json"
-import { SupportedFetch } from "./fetch-types"
 
 export interface ClientOptions {
   auth?: string
@@ -91,7 +89,6 @@ export interface ClientOptions {
   logLevel?: LogLevel
   logger?: Logger
   notionVersion?: string
-  fetch?: SupportedFetch
   /** Silently ignored in the browser */
   agent?: Agent
 }
@@ -121,7 +118,6 @@ export default class Client {
   #prefixUrl: string
   #timeoutMs: number
   #notionVersion: string
-  #fetch: SupportedFetch
   #agent: Agent | undefined
   #userAgent: string
 
@@ -134,7 +130,6 @@ export default class Client {
     this.#prefixUrl = (options?.baseUrl ?? "https://api.notion.com") + "/v1/"
     this.#timeoutMs = options?.timeoutMs ?? 60_000
     this.#notionVersion = options?.notionVersion ?? Client.defaultNotionVersion
-    this.#fetch = options?.fetch ?? nodeFetch
     this.#agent = options?.agent
     this.#userAgent = `notionhq-client/${PACKAGE_VERSION}`
   }
@@ -204,7 +199,7 @@ export default class Client {
     }
     try {
       const response = await RequestTimeoutError.rejectAfterTimeout(
-        this.#fetch(url.toString(), {
+        fetch(url.toString(), {
           method: method.toUpperCase(),
           headers,
           body: bodyAsJsonString,
