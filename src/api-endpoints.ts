0dc3247256bc2b96ad6467133f6cedd578f580d7
bansalji02 <hb13april2002@gmail.com>
2024-05-29 14:39:15 +0530

    added the request_id in oAuthResponse object

--- src/api-endpoints.ts
+++ src/api-endpoints.ts
@@ -11582,6 +11582,7 @@ export type OauthTokenResponse = {
       }
     | { type: "workspace"; workspace: true }
   duplicated_template_id: string | null
+  request_id:string
 }
 
 export const oauthToken = {
