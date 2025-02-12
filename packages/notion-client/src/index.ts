5fb90fad5b8c5ecc74fbec91fb46330e7bfdc8d0
Jacob Hands <jacob@gogit.io>
2025-02-12 05:31:15 -0600

    fix type issues

--- packages/notion-client/src/index.ts
+++ packages/notion-client/src/index.ts
@@ -1,17 +1,20 @@
 export { default as Client } from './Client'
-export { LogLevel, Logger } from './logging'
+export { LogLevel } from './logging'
+export type { Logger } from './logging'
 export {
-	// Error codes
-	NotionErrorCode,
 	APIErrorCode,
 	ClientErrorCode,
-	// Error types
-	NotionClientError,
 	APIResponseError,
 	UnknownHTTPResponseError,
 	RequestTimeoutError,
 	// Error helpers
 	isNotionClientError,
+} from './errors'
+export type {
+	// Error codes
+	NotionErrorCode,
+	// Error types
+	NotionClientError,
 } from './errors'
 export {
 	collectPaginatedAPI,
