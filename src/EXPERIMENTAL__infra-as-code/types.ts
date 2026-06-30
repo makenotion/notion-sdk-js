d00393272ff3285da243694ef840081968f3a610
Misha Reyes <misha.reyes@makenotion.com>
2026-06-30 18:07:55 -0400

    remove eslint disable

--- src/EXPERIMENTAL__infra-as-code/types.ts
+++ src/EXPERIMENTAL__infra-as-code/types.ts
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars, no-useless-escape */
-
 /* =============================================================================
 
 	GENERATED FILE - DO NOT EDIT MANUALLY
